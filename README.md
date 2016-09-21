5d6896b8ce23040c131ada7558ba0666c0cf5c8b
cvasseng <cvasseng@gmail.com>
2016-09-21 16:59:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Highcharts Node.js Export Server
+
+The export server can be ran as both a CLI converter, and as a HTTP server.
+
+## Install
+   
+   npm install highcharts-export-server -g
+
+## Running
+    
+    highcharts-export-server <arguments>
+
+## Command Line Arguments
+    
+    * `--infile`: Specify the input file.
+    * `--instr`: Specify the input as a string.
+    * `--outfile`: Specify the output filename.
+    * `--type`: The type of the exported file. Valid options are `jpg png pdf svg`.
+    * `--scale`: The scale of the chart.
+    * `--width`: Override the width of the chart.
+    * `--constr`: The constructor to use. Either `Chart` or `StockChart`.
+    * `--callback`: File containing JavaScript to call in the constructor of Highcharts.
+    * `--resources`: Stringified JSON.
+    * `--host`: The hostname to run a server on.
+    * `--port`: The port to listen for incoming requests on.
+    * `--tmpdir`: The path to temporary output files.
+
+## Injecting the Highcharts dependency
+
+Todo. Using CDN right now.
+
+## As a Node module
+
+Todo.
+
+## Server Test
+
+Run the below in a terminal after running `highcharts-export-server --enableServer`.
+    
+    # Generate a chart and save it to mychart.png    
+    curl -H "Content-Type: application/json" -X POST -d '{"infile":{"title": {"text": "Steep Chart"}, "xAxis": {"categories": ["Jan", "Feb", "Mar"]}, "series": [{"data": [29.9, 71.5, 106.4]}]}}' 127.0.0.1:7801 -o mychart.png
+
+## License
+
+[MIT](LICENSE).
---
+++ bin/cli.js
@@ -0,0 +1,110 @@
+#!/usr/bin/env node
+
+/*
+
+Highcharts Export Server
+
+Copyright (c) 2016, Highsoft
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+const main = require('./../lib/index');
+const args = process.argv;
+const fs = require('fs');
+
+var optionsMeta = {},
+    options = {}
+;
+
+////////////////////////////////////////////////////////////////////////////////
+
+function addOption(option, def, help) {
+    options[option] = def;
+    optionsMeta[option] = {
+        default: def,
+        help
+    };
+}
+
+//No lib for this one!
+function rpad(str, pad) {
+    pad = pad || 20;
+    if (str.length < pad) {
+        for (var i = str.length; i < pad; i++) {
+            str = str + '.';
+        }
+    }
+    return str;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+addOption('infile', false, 'the input file');
+addOption('outfile', false, 'the output filename');
+addOption('instr', false, 'an input chart JSON file. Overrides --infile.');
+addOption('type', 'png', 'the format to export to');
+addOption('scale', 1, 'the scale of the exported chart');
+addOption('width', false, 'the width of the exported chart, overrides chart settings');
+addOption('constr', 'Chart', 'the constructor to use. Either Chart or Stock.');
+addOption('callback', false, 'JavaScript file with code to run on construction');
+addOption('resources', {}, 'additional resource');
+addOption('host', '', 'start a server listening on the supplied hostname');
+addOption('port', 7801, 'server port');
+addOption('tmpdir', '/tmp/', 'path to temporary files');
+addOption('enableServer', false, 'start a server on 0.0.0.0');
+addOption('logLevel', 4, 'the log level. 0 = silent, 4 = verbose.');
+
+////////////////////////////////////////////////////////////////////////////////
+
+console.log(fs.readFileSync(__dirname + '/../msg/startup.msg').toString().bold.yellow);
+
+//Print usage if no arguments supplied
+if (args.length <= 2) {
+    console.log('Usage:'.bold);
+    
+    Object.keys(optionsMeta).forEach(function (option) {
+        console.log('  ' + rpad('--' + option), 
+                    optionsMeta[option].help, 
+                    'default:', 
+                    optionsMeta[option].default
+        );
+    });
+
+    return;
+}
+
+//Parse arguments
+//We can't use a foreach because we're parsing pairs.
+for (var i = 0; i < args.length; i++) {
+    var option = args[i].substr(2);
+
+    if (typeof options[option] !== 'undefined') {
+        options[option] = args[++i] || options[option];
+    }
+};
+
+if (options.enableServer || options.host.length) {
+    main.startServer(options.port);
+} else {
+    console.log('Running in CLI mode');
+    main.export(options);    
+}
