f33326d962e2e0a5affbacac4263936048e54d83
cvasseng <cvasseng@gmail.com>
2017-01-16 10:21:48 +0100

    Added module test

---
+++ tests/module/module_test.js
@@ -0,0 +1,63 @@
+//Include the exporter module
+const exporter = require('./../../lib/index.js');
+
+//Export settings 
+var exportSettings = {
+    type: 'png',
+    options: {
+        title: {
+            text: 'My Chart'
+        },
+        xAxis: {
+            categories: ["Jan", "Feb", "Mar", "Apr", "Mar", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        },
+        series: [
+            {
+                type: 'line',
+                data: [1, 3, 2, 4]
+            },
+            {
+                type: 'line',
+                data: [5, 3, 4, 2]
+            }
+        ],
+        plotOptions: {
+            series: {
+              dataLabels: {
+                enabled: true,
+                allowOverlap: true,
+                formatter: function () {
+                    var last = this.series.data[this.series.data.length - 1];
+                    if (this.point.category === last.category && this.point.y === last.y) {
+                      return this.series.name;
+                    }
+                    return "";
+                  }
+              }
+            }
+        }
+    }
+};
+
+//Set log level
+exporter.logLevel(4);
+
+//Set up a pool of PhantomJS workers
+exporter.initPool({
+    maxWorkers: 1
+});
+
+//Perform an export
+/*
+    Export settings corresponds to the available CLI arguments described
+    above.
+*/
+exporter.export(exportSettings, function (err, res) {
+    //The export result is now in res.
+    //If the output is not PDF or SVG, it will be base64 encoded (res.data).
+    //If the output is a PDF or SVG, it will contain a filename (res.filename).
+
+    //Kill the pool when we're done with it, and exit the application
+    exporter.killPool();
+    process.exit(1);
+});
