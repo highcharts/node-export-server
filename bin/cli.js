2d81981c479d72d0a92faa41f8cdd4f1e1dc69fb
PaulDalek <pawel.dalek@blacklabel.pl>
2022-01-04 18:53:04 +0100

    Let's await for pool to init in case direct export.

--- bin/cli.js
+++ bin/cli.js
@@ -33,136 +33,140 @@ const { printLogo, printUsage, pairArgumentValue } = require('../lib/utils');
 
 const { defaultConfig, nestedArgs } = require('../lib/schemas/config.js');
 
-const args = process.argv;
+// The main start function to start server or do the direct export
+const start = async () => {
+  const args = process.argv;
 
-// Set default values for server's options and returns them
-let options = initDefaultOptions(defaultConfig);
+  // Set default values for server's options and returns them
+  let options = initDefaultOptions(defaultConfig);
 
-// Print initial logo or text
-printLogo(options.other.noLogo);
+  // Print initial logo or text
+  printLogo(options.other.noLogo);
 
-// Print the usage information if no arguments supplied
-if (args.length <= 2) {
-  return printUsage(defaultConfig);
-}
-
-// Parse provided arguments
-options = pairArgumentValue(options, args, defaultConfig, nestedArgs);
-
-// If all options correctly parsed
-if (options) {
-  // In this case we want to prepare config manually
-  if (options.customCode.createConfig) {
-    return manualConfiguration(options.customCode.createConfig);
+  // Print the usage information if no arguments supplied
+  if (args.length <= 2) {
+    return printUsage(defaultConfig);
   }
 
-  // Start server
-  if (options.server.enable) {
-    // Init a pool for the server and send options
-    main.initPool(options);
+  // Parse provided arguments
+  options = pairArgumentValue(options, args, defaultConfig, nestedArgs);
 
-    // Run the server
-    main.startServer(options.server);
-  } else {
-    // Try to load resources from a file
-    if (!options.customCode.resources) {
-      try {
-        options.resources = JSON.parse(readFileSync('resources.json', 'utf8'));
-      } catch (notice) {
-        main.log(3, `[cli] - No resources found.`);
-      }
+  // If all options correctly parsed
+  if (options) {
+    // In this case we want to prepare config manually
+    if (options.customCode.createConfig) {
+      return manualConfiguration(options.customCode.createConfig);
     }
 
-    // Perform batch exports
-    if (options.export.batch) {
-      const batchFunctions = [];
-
-      // If not set explicitly, use default option for batch exports
-      if (!args.includes('--initialWorkers', '--maxWorkers')) {
-        options.pool = {
-          ...options.pool,
-          initialWorkers: 5,
-          maxWorkers: 25,
-          reaper: false
-        };
-      }
-
-      // Init a pool for the batch exports
+    // Start server
+    if (options.server.enable) {
+      // Init a pool for the server and send options
       main.initPool(options);
 
-      // Split and pair the --batch arguments
-      for (let pair of options.export.batch.split(';')) {
-        pair = pair.split('=');
-        if (pair.length === 2) {
-          batchFunctions.push(
-            new Promise((resolve) => {
-              main.startExport(
-                {
-                  ...options,
-                  export: {
-                    ...options.export,
-                    infile: pair[0],
-                    outfile: pair[1]
-                  }
-                },
-                (data, error) => {
-                  // Throw an error
-                  if (error) {
-                    throw error;
-                  }
-
-                  // Save the base64 from a buffer to a correct image file
-                  writeFileSync(
-                    data.options.export.outfile,
-                    Buffer.from(data.result.data, 'base64')
-                  );
-
-                  resolve();
-                }
-              );
-            })
+      // Run the server
+      main.startServer(options.server);
+    } else {
+      // Try to load resources from a file
+      if (!options.customCode.resources) {
+        try {
+          options.customCode.resources = JSON.parse(
+            readFileSync('resources.json', 'utf8')
           );
+        } catch (notice) {
+          main.log(3, `[cli] - No resources found.`);
         }
       }
 
-      // Kill the pool after all exports are done
-      Promise.all(batchFunctions).then(() => {
-        main.killPool();
-      });
-    } else {
-      // Or simply export chart through CLI
-      // If not set explicitly, use default option for a single export
-      if (!args.includes('--initialWorkers', '--maxWorkers')) {
+      // Perform batch exports
+      if (options.export.batch) {
+        const batchFunctions = [];
+
+        // If not set explicitly, use default option for batch exports
+        if (!args.includes('--initialWorkers', '--maxWorkers')) {
+          options.pool = {
+            ...options.pool,
+            initialWorkers: 5,
+            maxWorkers: 25,
+            reaper: false
+          };
+        }
+
+        // Init a pool for the batch exports
+        await main.initPool(options);
+
+        // Split and pair the --batch arguments
+        for (let pair of options.export.batch.split(';')) {
+          pair = pair.split('=');
+          if (pair.length === 2) {
+            batchFunctions.push(
+              new Promise((resolve) => {
+                main.startExport(
+                  {
+                    ...options,
+                    export: {
+                      ...options.export,
+                      infile: pair[0],
+                      outfile: pair[1]
+                    }
+                  },
+                  (data, error) => {
+                    // Throw an error
+                    if (error) {
+                      throw error;
+                    }
+
+                    // Save the base64 from a buffer to a correct image file
+                    writeFileSync(
+                      data.options.export.outfile,
+                      Buffer.from(data.result.data, 'base64')
+                    );
+
+                    resolve();
+                  }
+                );
+              })
+            );
+          }
+        }
+
+        // Kill the pool after all exports are done
+        Promise.all(batchFunctions).then(() => {
+          main.killPool();
+        });
+      } else {
+        // No need for multiple workers in case of a single CLI export
         options.pool = {
           ...options.pool,
           initialWorkers: 1,
           maxWorkers: 1,
           reaper: false
         };
+
+        // Init a pool for one export
+        await main.initPool(options);
+
+        // Use instr or its alias, options
+        options.export.instr = options.export.instr || options.export.options;
+
+        // Perform an export
+        main.startExport(options, (data, error) => {
+          // Throw an error
+          if (error) {
+            throw error;
+          }
+
+          // Save the base64 from a buffer to a correct image file
+          writeFileSync(
+            data.options.export.outfile,
+            Buffer.from(data.result.data, 'base64')
+          );
+
+          // Kill the pool
+          main.killPool();
+        });
       }
-
-      // Init a pool for one export
-      main.initPool(options);
-
-      // Use instr or its alias, options
-      options.export.instr = options.export.instr || options.export.options;
-
-      // Perform an export
-      main.startExport(options, (data, error) => {
-        // Throw an error
-        if (error) {
-          throw error;
-        }
-
-        // Save the base64 from a buffer to a correct image file
-        writeFileSync(
-          data.options.export.outfile,
-          Buffer.from(data.result.data, 'base64')
-        );
-
-        // Kill the pool
-        main.killPool();
-      });
     }
   }
-}
+};
+
+start();
