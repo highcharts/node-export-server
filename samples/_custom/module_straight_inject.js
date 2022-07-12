c8d62922085e0942a9f02f2cfc28bbe5666b1a96
PaulDalek <pawel.dalek@blacklabel.pl>
2022-07-12 20:28:18 +0200

    Corrected the way straight inject from within options works.

--- samples/_custom/module_straight_inject.js
+++ samples/_custom/module_straight_inject.js
@@ -18,7 +18,7 @@ const exportSettings = {
           dataLabels: {
             enabled: true,
             formatter: function () {
-              return `${this.series.name}${this.y}`;
+              return this.series.name + '' + this.y;
             }
           }
         }
