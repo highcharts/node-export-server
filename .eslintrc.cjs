c0fde728cf4caa1231634d504aa5a872e1633204
Jakub Szumiński <jakubszuminskixrp@gmail.com>
2023-10-30 12:11:03 +0100

    Changed tabs in ESLint config.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,27 @@
 module.exports = {
-    env: {
-      es2021: true,
-      node: true
-    },
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module'
-    },
-    extends: [
-      'eslint:recommended',
-      'plugin:import/recommended',
-      'plugin:prettier/recommended'
-    ],
-    plugins: ['import', 'prettier'],
-    rules: {
-      'no-unused-vars': 'off',
-      'import/no-cycle': 2,
-      'prettier/prettier': [
-        'error',
-        {
-          singleQuote: true,
-          trailingComma: 'none'
-        }
-      ]
-    }
+  env: {
+    es2021: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:prettier/recommended'
+  ],
+  plugins: ['import', 'prettier'],
+  rules: {
+    'no-unused-vars': 'off',
+    'import/no-cycle': 2,
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'none'
+      }
+    ]
+  }
 }
