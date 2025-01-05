015af527a532414a509762054ab3613e9e6fb83a
PaulDalek <pawel.dalek@blacklabel.pl>
2025-01-05 22:01:02 +0100

    Optimization of the cli module.

--- bin/cli.js
+++ bin/cli.js
@@ -3,7 +3,7 @@
 
 Highcharts Export Server
 
-Copyright (c) 2016-2024, Highsoft
+Copyright (c) 2016-2025, Highsoft
 
 Licenced under the MIT licence.
 
@@ -22,69 +22,76 @@ See LICENSE file in root for details.
  */
 
 import { singleExport, batchExport } from '../lib/chart.js';
-import { setOptions } from '../lib/config.js';
+import {
+  printLicense,
+  printUsage,
+  printVersion,
+  setOptions
+} from '../lib/config.js';
 import { initExport } from '../lib/index.js';
 import { log, logWithStack } from '../lib/logger.js';
 import { manualConfig } from '../lib/prompt.js';
 import { shutdownCleanUp } from '../lib/resourceRelease.js';
-import { printLicense, printVersion } from '../lib/utils.js';
-import { printUsage } from '../lib/schemas/config.js';
 import { startServer } from '../lib/server/server.js';
 
 import ExportError from '../lib/errors/ExportError.js';
 
 /**
  * The primary function to initiate the server or perform the direct export.
- * Logs an error if it occurs during the execution and gracefully shut down
+ * Logs an error if it occurs during the execution and gracefully shuts down
  * the process.
  *
  * @async
  * @function start
  *
- * @throws {ExportError} Throws an `ExportError` if no valid options are
- * provided.
+ * @returns {Promise<void>} A Promise that resolves to ending the function
+ * execution when displaying license, version, or usage information, or when
+ * triggering manual configuration using the prompts functionality.
+ *
+ * @throws {ExportError} Throws an `ExportError` if no valid options
+ * are provided.
  */
 async function start() {
   try {
     // Get the CLI arguments
     const args = process.argv;
 
-    // Display license information if requested
-    if (['-v', '--v'].includes(args[args.length - 1])) {
-      // Print logo with the version and license information
-      return printLicense();
+    // Display version and license information if requested
+    if (['-v', '--v'].some((a) => args.includes(a))) {
+      // Print logo with version and license information
+      printLicense();
+      return;
     }
 
     // Display help information if requested
-    if (['-h', '--h', '-help', '--help'].includes(args[args.length - 1])) {
+    if (['-h', '--h', '-help', '--help'].some((a) => args.includes(a))) {
       // Print CLI usage information
-      return printUsage();
-    }
-
-    // Print CLI usage information if no arguments supplied
-    if (args.length <= 2) {
       printUsage();
-      return log(
-        2,
-        '[cli] The number of provided arguments is too small. Please refer to the help section above.'
-      );
+      return;
     }
 
-    // Set the options, keeping the priority order of setting values:
-    // 1. Options from the `lib/schemas/config.js` file
-    // 2. Options from a custom JSON file (loaded by the `loadConfig` argument)
-    // 3. Options from the environment variables (the `.env` file)
-    // 4. Options from the CLI
-    const options = setOptions(null, args, true);
+    // Print CLI usage information if no arguments are supplied
+    if (args.length <= 2) {
+      printVersion();
+      log(
+        2,
+        '[cli] The number of provided arguments is too small. Please refer to the help section (use --h or --help command).'
+      );
+      return;
+    }
 
-    // If all options correctly parsed
+    // Set the options, keeping the priority order of setting values
+    const options = setOptions({}, args, true);
+
+    // If all options are correctly parsed
     if (options) {
       // Print initial logo or text with the version
       printVersion(options.other.noLogo);
 
       // In this case we want to prepare config manually
       if (options.customLogic.createConfig) {
-        return manualConfig(options.customLogic.createConfig);
+        manualConfig(options.customLogic.createConfig);
+        return;
       }
 
       // Start server
@@ -97,7 +104,7 @@ async function start() {
       } else {
         // Perform batch exports
         if (options.export.batch) {
-          // Init a pool for the batch exports
+          // Init the export mechanism for batch exports
           await initExport(options);
 
           // Start batch exports
@@ -107,7 +114,7 @@ async function start() {
           options.pool.minWorkers = 1;
           options.pool.maxWorkers = 1;
 
-          // Init a pool for one export
+          // Init the export mechanism for a single export
           await initExport(options);
 
           // Start a single export
