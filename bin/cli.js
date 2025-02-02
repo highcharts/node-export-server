809f6a2d2d6e239e1014b403cdcff340ff87c964
PaulDalek <pawel.dalek@blacklabel.pl>
2025-02-03 00:24:19 +0100

    Optimized, reduced and moved info-related functions into a separate module.

--- bin/cli.js
+++ bin/cli.js
@@ -16,19 +16,16 @@ See LICENSE file in root for details.
 /**
  * @overview This module serves as the entry point for the Highcharts Export
  * Server. It provides functionality for starting the server, performing batch
- * or single chart exports, and managing configuration options. It supports both
- * CLI and server-based usage, allowing flexible integration for generating
- * Highcharts exports in various environments.
+ * or single chart exports, managing manual configuration options through
+ * the prompts functionality, and updating options with values from the passed
+ * CLI arguments. It supports both CLI and server-based usage, enabling
+ * integration for generating Highcharts exports in various environments.
  */
 
-import { singleExport, batchExport } from '../lib/chart.js';
-import {
-  printLicense,
-  printUsage,
-  printVersion,
-  setCliOptions
-} from '../lib/config.js';
+import { batchExport, singleExport } from '../lib/chart.js';
+import { setCliOptions } from '../lib/config.js';
 import { initExport } from '../lib/index.js';
+import { printInfo, printUsage } from '../lib/info.js';
 import { log, logWithStack } from '../lib/logger.js';
 import { manualConfig } from '../lib/prompt.js';
 import { shutdownCleanUp } from '../lib/resourceRelease.js';
@@ -38,16 +35,17 @@ import { startServer } from '../lib/server/server.js';
 import ExportError from '../lib/errors/ExportError.js';
 
 /**
- * The primary function to initiate the server or perform the direct export.
+ * The primary function to initiate the server or perform the direct CLI export.
  * Logs an error if it occurs during the execution and gracefully shuts down
  * the process.
  *
  * @async
  * @function start
  *
- * @returns {Promise<void>} A Promise that resolves to ending the function
- * execution when displaying license, version, or usage information, or when
- * triggering manual configuration using the prompts functionality.
+ * @returns {Promise<void>} A Promise that resolves when the function execution
+ * ends after displaying the logo with license and version information, showing
+ * the CLI usage details, or triggering manual configuration using the prompts
+ * functionality.
  *
  * @throws {ExportError} Throws an `ExportError` if no valid options
  * are provided.
@@ -57,10 +55,21 @@ async function start() {
     // Get the CLI arguments
     const args = process.argv;
 
+    // If no arguments are supplied
+    if (args.length <= 2) {
+      // Print logo with version and license information
+      printInfo();
+      log(
+        2,
+        '[cli] The number of provided arguments is too small. Please refer to the help section (use --h or --help command).'
+      );
+      return;
+    }
+
     // Display version and license information if requested
     if (['-v', '--v'].some((a) => args.includes(a))) {
       // Print logo with version and license information
-      printLicense();
+      printInfo();
       return;
     }
 
@@ -71,23 +80,13 @@ async function start() {
       return;
     }
 
-    // Print CLI usage information if no arguments are supplied
-    if (args.length <= 2) {
-      printVersion();
-      log(
-        2,
-        '[cli] The number of provided arguments is too small. Please refer to the help section (use --h or --help command).'
-      );
-      return;
-    }
-
     // Set the options from CLI, keeping the priority order of setting values
     const options = setCliOptions(args);
 
     // If all options are correctly parsed
     if (options) {
-      // Print initial logo or text with the version
-      printVersion(options.other.noLogo);
+      // Print initial logo or text with the version information
+      printInfo(options.other.noLogo, true);
 
       // In this case we want to prepare config manually
       if (options.customLogic.createConfig) {
@@ -133,7 +132,7 @@ async function start() {
       }
     } else {
       throw new ExportError(
-        '[cli] No valid options provided. Please check your input and try again.',
+        '[cli] No valid options found. Please check your input and try again.',
         400
       );
     }
